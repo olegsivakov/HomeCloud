f06ad639253c264cbacefde9e3068adaf0c4e732
Staravoitau, Aleh <A_Staravoitau@Medpace.com>
2017-10-20 01:35:30 +0300

    Data Storage: Added initial data contracts.

---
+++ DataStorage/HomeCloud.DataStorage.DataAccess.Contracts/Storage.cs
@@ -0,0 +1,54 @@
+﻿namespace HomeCloud.DataStorage.DataAccess.Contracts
+{
+	#region Usings
+
+	using System;
+
+	#endregion
+
+	/// <summary>
+	/// Represents <see cref="Storage" /> data contract.
+	/// </summary>
+	public class Storage
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		/// <value>
+		/// The identifier.
+		/// </value>
+		public Guid ID { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>
+		/// The name.
+		/// </value>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the quota.
+		/// </summary>
+		/// <value>
+		/// The quota.
+		/// </value>
+		public long Quota { get; set; }
+
+		/// <summary>
+		/// Gets or sets the creation date.
+		/// </summary>
+		/// <value>
+		/// The creation date.
+		/// </value>
+		public DateTime CreationDate { get; set; }
+
+		/// <summary>
+		/// Gets or sets the updated date.
+		/// </summary>
+		/// <value>
+		/// The updated date.
+		/// </value>
+		public DateTime UpdatedDate { get; set; }
+	}
+}
