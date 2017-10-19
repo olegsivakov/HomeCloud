92a3a2b11b197d1cf57a526dee3c1cad00212553
Staravoitau, Aleh <A_Staravoitau@Medpace.com>
2017-10-20 00:46:46 +0300

    DataStorage: Added 'delete' stored procedures.

---
+++ Databases/HomeCloud.DataStorage/StoredProcedures/DeleteDirectoryByParentID.sql
@@ -0,0 +1,37 @@
+﻿IF EXISTS (SELECT 1 FROM sys.procedures WHERE [name] = 'DeleteDirectoryByParentID')
+BEGIN
+	DROP PROCEDURE [dbo].[DeleteDirectoryByParentID]
+END
+GO
+
+CREATE PROCEDURE [dbo].[DeleteDirectoryByParentID]
+	@ParentID UNIQUEIDENTIFIER
+AS
+BEGIN
+	DECLARE @local_ParentID UNIQUEIDENTIFIER = @ParentID
+
+	;WITH RowsToDelete AS (
+		SELECT
+			[ID]
+		FROM [dbo].[Directory]
+		WHERE
+			[ParentID] = @local_ParentID
+
+		UNION ALL
+
+		SELECT
+			children.[ID]
+		FROM [dbo].[Directory] AS children
+		INNER JOIN RowsToDelete AS parent ON parent.[ID] = children.[ParentID]
+	)
+
+	DELETE FROM [dbo].[File]
+	FROM [dbo].[File] [file]
+	INNER JOIN RowsToDelete rowsToDelete ON [file].[DirectoryID] = rowsToDelete.[ID]
+
+	DELETE FROM [dbo].[Directory]
+	FROM [dbo].[Directory] directory
+	INNER JOIN RowsToDelete rowsToDelete ON directory.[ID] = rowsToDelete.[ID]
+END
+GO
+
---
+++ Databases/HomeCloud.DataStorage/StoredProcedures/DeleteStorageByID.sql
@@ -0,0 +1,24 @@
+﻿IF EXISTS (SELECT 1 FROM sys.procedures WHERE [name] = 'DeleteStorageByID')
+BEGIN
+	DROP PROCEDURE [dbo].[DeleteStorageByID]
+END
+GO
+
+CREATE PROCEDURE [dbo].[DeleteStorageByID]
+	@ID UNIQUEIDENTIFIER
+AS
+BEGIN
+	DECLARE @local_ID UNIQUEIDENTIFIER = @ID
+
+	DELETE FROM [dbo].[File]
+	FROM [dbo].[File] [file]
+	INNER JOIN [dbo].[Directory] directory ON directory.[ID] = [file].[DirectoryID]
+	WHERE
+		directory.[StorageID] = @local_ID
+
+	DELETE FROM [dbo].[Directory]
+	WHERE
+		[StorageID] = @local_ID
+END
+GO
+
