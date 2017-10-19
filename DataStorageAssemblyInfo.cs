cbac3a55289d04de9462601f76e792bf76ffa905
Staravoitau, Aleh <A_Staravoitau@Medpace.com>
2017-10-19 21:41:49 +0300

    Added Data Storage Global Assembly Info.

---
+++ DataStorageAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyCompany("olegsivakov")]
+[assembly: AssemblyProduct("Home Data Storage Application")]
+[assembly: AssemblyCopyright("Copyright © olegsivakov 2017")]
+[assembly: AssemblyTrademark("Home Cloud Data Storage")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
