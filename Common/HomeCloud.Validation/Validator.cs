ab9dabb3e84ea48d286567e5a88f1dfc8fc85da2
aleh.sivakou <aleh.sivakou@itechart-group.com>
2017-10-30 17:42:47 +0300

    Implementing validation component.

---
+++ Common/HomeCloud.Validation/Validator.cs
@@ -0,0 +1,40 @@
+﻿namespace HomeCloud.Validation
+{
+	#region Usings
+
+	using System;
+	using System.Collections.Generic;
+
+	#endregion
+
+	public abstract class Validator<T> : IValidator<T>
+	{
+		#region Private Members
+
+		private readonly IList<IValidationRule<T>> rules = new List<IValidationRule<T>>();
+
+		#endregion
+
+		#region IValidator<T> Implementations
+
+		public ValidationResult Validate(T entity)
+		{
+			return new ValidationResult();
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		protected IValidationRule<T> If(Func<T, bool> rule)
+		{
+			IValidationRule<T> result = new ValidationRule<T>(rule);
+
+			this.rules.Add(result);
+
+			return result;
+		}
+
+		#endregion
+	}
+}
