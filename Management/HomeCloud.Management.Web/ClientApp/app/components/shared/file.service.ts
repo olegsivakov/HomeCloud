598bdeaa70bc79ecc6559084d0f5ee9be9e1c195
aleh.sivakou <aleh.sivakou@itechart-group.com>
2017-12-01 17:42:54 +0300

    Management UI: implementing services.

---
+++ Management/HomeCloud.Management.Web/ClientApp/app/components/shared/file.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class FileService {
+	constructor(
+		private http: Http
+	) { }
+
+	uploadFile() {
+
+	}
+
+	getFiles(catalogId: string, offset: number, limit: number) {
+	}
+
+
+}
+
