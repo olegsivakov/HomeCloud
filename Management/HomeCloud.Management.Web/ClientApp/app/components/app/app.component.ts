24370a065d98e8e227c6dcd43d5fedd97eaa4db0
oleg.sivakov <sivakovoleg@yahoo.com>
2017-12-05 02:29:51 +0300

    Management UI: designing UI.

--- Management/HomeCloud.Management.Web/ClientApp/app/components/app/app.component.ts
+++ Management/HomeCloud.Management.Web/ClientApp/app/components/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
     styleUrls: [ './../../assets/themes/blue.css', './app.component.css' ]
 })
 export class AppComponent {
+	public _opened: boolean = false;
+
+	public _toggleSidebar() {
+		this._opened = !this._opened;
+	}
 }
