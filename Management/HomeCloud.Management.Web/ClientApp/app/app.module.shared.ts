37be2dea5056edc14684ff7d43816ffcc6316390
Oleg Sivakov <sivakovoleg@yahoo.com>
2018-01-23 01:59:03 +0300

    Implemented notification bar.

--- Management/HomeCloud.Management.Web/ClientApp/app/app.module.shared.ts
+++ Management/HomeCloud.Management.Web/ClientApp/app/app.module.shared.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './components/app/app.component';
+import { NotificationComponent } from './components/notification/notification.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { UserProfileBarComponent } from './components/user-profile-bar/user-profile-bar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,10 +17,12 @@ import { SidebarModule } from 'ng-sidebar';
 import { ExpandableListModule } from 'angular2-expandable-list';
 import { FileUploadModule } from 'ng2-file-upload';
 import { ContextmenuModule } from 'ng2-contextmenu';
+import { ToasterModule } from 'angular2-toaster';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		NotificationComponent,
 		NavMenuComponent,
 		UserProfileBarComponent,
 		HomeComponent,
@@ -35,12 +38,16 @@ import { ContextmenuModule } from 'ng2-contextmenu';
 		ExpandableListModule,
 		FileUploadModule,
 		ContextmenuModule,
+		ToasterModule,
 		RouterModule.forRoot([
 			{ path: '', redirectTo: 'home', pathMatch: 'full' },
 			{ path: 'home', component: HomeComponent },
 			{ path: 'catalogs', component: DataComponent },
 			{ path: '**', redirectTo: 'home' }
 		])
+	],
+	entryComponents: [
+		NotificationComponent
 	]
 })
 export class AppModuleShared {
---
+++ Management/HomeCloud.Management.Web/ClientApp/app/components/notification/notification.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'notification',
+	templateUrl: './notification.component.html',
+	styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+
+	public toast: Notification;
+
+	constructor() {
+	}
+}
