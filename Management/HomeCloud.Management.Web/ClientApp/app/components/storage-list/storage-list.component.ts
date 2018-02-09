e152b3b21add8398494b39e633d8792cd33d8f12
aleh.sivakou <aleh.sivakou@itechart-group.com>
2018-02-09 15:01:49 +0300

    Redesigned file storage.

--- Management/HomeCloud.Management.Web/ClientApp/app/components/storage-list/storage-list.component.ts
+++ Management/HomeCloud.Management.Web/ClientApp/app/components/storage-list/storage-list.component.ts
@@ -19,9 +19,9 @@ export class StorageListComponent implements OnInit {
 	public get(): Array<Storage> {
 		let storages = new Array<Storage>();
 
-		storages[0] = this.getStorage("Aleh Sivakou's storage", 295068491776, 1000202039296);
-		storages[1] = this.getStorage("My wife's storage", 730147488686, 1000202039296);
-		storages[2] = this.getStorage("My daugther's storage", 73767122944, 147534245888);
+		storages[0] = this.getStorage("Storage 1", 295068491776, 1000202039296);
+		storages[1] = this.getStorage("Storage 2", 730147488686, 1000202039296);
+		storages[2] = this.getStorage("Storage 3", 73767122944, 147534245888);
 
 		return storages;
 	}
