1de9f50be08a4ed488931d77495fc9dca76e0973
Oleg Sivakov <sivakovoleg@yahoo.com>
2018-05-10 00:13:20 +0300

    Implementing identity service: data access.

---
+++ IdentityService/HomeCloud.IdentityService.DataAccess.Objects/ResourceDocument.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeCloud.IdentityService.DataAccess.Objects
+{
+	/// <summary>
+	/// Represents <see cref="api"/> resource application document.
+	/// </summary>
+	/// <seealso cref="Object" />
+	public class ResourceDocument
+	{
+	}
+}
