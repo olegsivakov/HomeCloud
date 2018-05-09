8b8228a6d156f5f8b35d48133a901c72cbdbe70a
Oleg Sivakov <sivakovoleg@yahoo.com>
2018-05-10 00:13:20 +0300

    Implementing identity service: data access.

---
+++ IdentityService/HomeCloud.IdentityService.DataAccess.Objects/ClientDocument.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace HomeCloud.IdentityService.DataAccess.Objects
+{
+	/// <summary>
+	/// Represents client application document.
+	/// </summary>
+	public class ClientDocument
+	{
+		public string ID { get; set; }
+
+		public string Name { get; set; }
+
+		public IEnumerable<string> GrantTypes { get; set; }
+
+		public IEnumerable<string> RedirectUris { get; set; }
+
+		public IEnumerable<string> PostLogoutRedirectUris { get; set; }
+
+		public IEnumerable<string> Origins { get; set; }
+
+		public IEnumerable<string> Scopes { get; set; }
+
+		public int IdentityTokenLifetime { get; set; }
+
+		public int AccessTokenLifetime { get; set; }
+
+		public int AbsoluteRefreshTokenLifetime { get; set; }
+
+		public int SlidingRefreshTokenLifetime { get; set; }
+	}
+}
